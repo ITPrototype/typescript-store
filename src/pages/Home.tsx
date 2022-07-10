5d0c9a8356969c629c51dbb4928019f35136e843
ITPrototype <20020503kamron@gmail.com>
2022-07-10 13:28:13 +0500

    bug fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import '../styles/intro.css'
 import whyus from '../image/why-us.png'
+import { NavLink } from 'react-router-dom'
 
 
 export default function Home() {
@@ -12,8 +13,8 @@ export default function Home() {
           <hr />
           <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
           <div className="buttons">
-            <a href="/store" className="btn-get-started scrollto"><span>Get Started</span><i className="fa-solid fa-play"></i></a>
-            <a href="" className="glightbox btn-watch-video"><span>Github</span> <i className="fa-brands fa-github"></i></a>
+            <NavLink to="/store" className="btn-get-started scrollto"><span>Get Started</span><i className="fa-solid fa-play"></i></NavLink>
+            <a href="https://github.com/ITPrototype" className="glightbox btn-watch-video"><span>Github</span> <i className="fa-brands fa-github"></i></a>
           </div>
         </div>
       </div>
