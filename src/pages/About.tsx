dc742ba0a1c3f5f23384edafdb46b5a7086a7e81
Kamron Mamedov <94013172+ITPrototype@users.noreply.github.com>
2022-09-03 00:29:45 +0500

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,7 +12,7 @@ export default function About() {
             <li>For this project i used : React,TypeScript,React-Bootstrap and <a href="https://fakestoreapi.com" target="_blank">FakeStore Api</a></li>
             <li>Also check my Github <a href="https://github.com/ITPrototype" target="_blank">Link</a></li>
             <ul>
-              <li>👉My <a href="https://instagram.com/mamedov.kemrannn" target="_blank" style={{color:'red'}}>Instagram <i className="fa-brands fa-instagram"></i></a></li>
+              <li>👉My <a href="https://instagram.com/mamedov.kamronnn" target="_blank" style={{color:'red'}}>Instagram <i className="fa-brands fa-instagram"></i></a></li>
               <li>👉My <a href="https://www.linkedin.com/in/kamron-mamedov-177396237/" target="_blank" style={{color:'blue'}}>LinkedIn <i className="fa-brands fa-linkedin"></i></a></li>
               <li>👉My <a href="https://t.me/DjdgsuTbshsgK12" target="_blank" style={{color:'lightblue'}}>Telegram <i className="fa-brands fa-telegram"></i></a></li>
             </ul>
